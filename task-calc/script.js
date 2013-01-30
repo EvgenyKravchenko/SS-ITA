092586160eeaa15d06cbf451360c0278b6de7cd6
Evgeny Kravchenko <evgeniy.kravtschenko@fmail.com>
2013-01-30 11:54:17 +0200

    Added class Calc

--- task-calc/script.js
+++ task-calc/script.js
@@ -1,63 +1,96 @@
 window.onload = function() {
-    document.getElementById("add").addEventListener('click', calculate, false);
-    document.getElementById("sub").addEventListener('click', calculate, false);
-    document.getElementById("mul").addEventListener('click', calculate, false);
-    document.getElementById("div").addEventListener('click', calculate, false);
+    
+  function Calc() {
+    
+    this.add = function() {
+      this.action("+");
+    }
+
+    this.sub = function() {
+      this.action("-");
+    }
+
+    this.mul = function() {
+      this.action("*");
+    }
+
+    this.div = function() {
+      this.action("/");
+    }
+
+    this.action = function(action) {
+      this.get_values();
+
+      switch (action) {
+        case '+': this.result = this.operand1 + this.operand2;  break;
+        case '-': this.result = this.operand1 - this.operand2;  break;
+        case '*': this.result = this.operand1 * this.operand2;  break;
+        case '/': this.result = this.operand1 / this.operand2;  break;
+      }
+
+      this.view_result();
+    }
+
+    this.get_values = function() {
+      this.operand1 = Number(document.getElementById("operand1").value);
+      this.operand2 = Number(document.getElementById("operand2").value);
+    }
+
+    this.view_result = function() {
+      this.validate_result();
+      document.getElementById("result").value = this.result;
+    }
+
+    this.validate_result = function() {
+      this.result = (this.result === Infinity || isNaN(this.result) === true) ? 'Div by zero!': this.result.toFixed(2);
+    }
+
+  }
+
+
+    calc = new Calc();
+    
+    document.getElementById("add").addEventListener('click', function(){calc.add();}, false);
+    document.getElementById("sub").addEventListener('click', function(){calc.sub();}, false);
+    document.getElementById("mul").addEventListener('click', function(){calc.mul();}, false);
+    document.getElementById("div").addEventListener('click', function(){calc.div();}, false);
     document.getElementById("result").readOnly = true;
+    
     disableArray = new Array(0,0);
-    checkValues(document.getElementById("operand1"), 0);
     document.getElementById("operand1").addEventListener('keyup', function(){checkValues(this,0)}, false);
     document.getElementById("operand1").addEventListener('keyup', function(){checkValues(this,1)}, false);
 
 
-}
 
 
-function calculate() {
-    var op1, op2;
-    op1 = Number(document.getElementById("operand1").value);
-    op2 = Number(document.getElementById("operand2").value);
 
-    function process(result, action) {
-        document.getElementById("action").innerText = action;
-        document.getElementById("result").value = (result === Infinity || isNaN(result) === true) ? 'Div by zero!': result.toFixed(2);
-    }
-
-    var action = this.innerText;
-    switch (action) {
-        case '+': process(op1+op2, action); break;
-        case '-': process(op1-op2, action); break;
-        case 'x': process(op1*op2, action); break;
-        case '/': process(op1/op2, action); break;
-
-    }
 }
 
 
 function checkValues(obj, tag){
 
-    var regex = new RegExp(/^(?:\d+(?:\.\d+)?)$/);
-    if (regex.test(obj.value) || obj.value === '') {
-        obj.className = '';
-        disableArray[tag] = 1;
-    } else {
-        obj.className = 'wrong';
-        disableArray[tag] = 0;
-    }
+  var regex = new RegExp(/^(?:\d+(?:\.\d+)?)$/);
+  if (regex.test(obj.value) || obj.value === '') {
+      obj.className = '';
+      disableArray[tag] = 1;
+  } else {
+      obj.className = 'wrong';
+      disableArray[tag] = 0;
+  }
 
 
-    function setState(state) {
-        document.getElementById('add').disabled = state;
-        document.getElementById('sub').disabled = state;
-        document.getElementById('mul').disabled = state;
-        document.getElementById('div').disabled = state;
-    }
+  function setState(state) {
+      document.getElementById('add').disabled = state;
+      document.getElementById('sub').disabled = state;
+      document.getElementById('mul').disabled = state;
+      document.getElementById('div').disabled = state;
+  }
 
-    if (disableArray[0] === 1 && disableArray[1] === 1 && obj.value !== '') {
-        setState(false);
-    } else {
-        setState(true);
-    }
+  if (disableArray[0] === 1 && disableArray[1] === 1 && obj.value !== '') {
+      setState(false);
+  } else {
+      setState(true);
+  }
 
 }
 
