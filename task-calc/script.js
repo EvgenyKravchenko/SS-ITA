3fd190af2a12fb740b158a919f3a52f88fe3478f
Evgeny Kravchenko <evgeniy.kravtschenko@gmail.com>
2013-01-30 17:37:07 -0500

    Refactor for MVC pattern

--- task-calc/script.js
+++ task-calc/script.js
@@ -1,69 +1,16 @@
 window.onload = function() {
-    
-  function Calc() {
-    
-    this.add = function() {
-      this.action("+");
-    }
-
-    this.sub = function() {
-      this.action("-");
-    }
-
-    this.mul = function() {
-      this.action("*");
-    }
-
-    this.div = function() {
-      this.action("/");
-    }
-
-    this.action = function(action) {
-      this.get_values();
-
-      switch (action) {
-        case '+': this.result = this.operand1 + this.operand2;  break;
-        case '-': this.result = this.operand1 - this.operand2;  break;
-        case '*': this.result = this.operand1 * this.operand2;  break;
-        case '/': this.result = this.operand1 / this.operand2;  break;
-      }
-
-      this.view_result();
-    }
-
-    this.get_values = function() {
-      this.operand1 = parseFloat(document.getElementById("operand1").value);
-      this.operand2 = parseFloat(document.getElementById("operand2").value);
-    }
-
-    this.view_result = function() {
-      this.validate_result();
-      document.getElementById("result").value = this.result;
-    }
-
-    this.validate_result = function() {
-      this.result = (this.result === Infinity || isNaN(this.result) === true) ? 'Div by zero!': this.result.toFixed(2);
-    }
-    
-    return this;
-  }
-
-
-    var calc = new Calc();
-    
-    document.getElementById("add").addEventListener('click', function(){calc.add();}, false);
-    document.getElementById("sub").addEventListener('click', function(){calc.sub();}, false);
-    document.getElementById("mul").addEventListener('click', function(){calc.mul();}, false);
-    document.getElementById("div").addEventListener('click', function(){calc.div();}, false);
-    document.getElementById("result").readOnly = true;
-    
-    disableArray = new Array(0,0);
-    document.getElementById("operand1").addEventListener('keyup', function(){checkValues(this,0)}, false);
-    document.getElementById("operand1").addEventListener('keyup', function(){checkValues(this,1)}, false);
-
-
-
-
+  
+  var calc = new Calc();
+  document.getElementById("add").addEventListener('click', function(){calc.add();}, false);
+  document.getElementById("sub").addEventListener('click', function(){calc.sub();}, false);
+  document.getElementById("mul").addEventListener('click', function(){calc.mul();}, false);
+  document.getElementById("div").addEventListener('click', function(){calc.div();}, false);
+  document.getElementById("result").readOnly = true;
+  
+  disableArray = new Array(0, 0);
+  checkValues(document.getElementById("operand1"), 0);
+  document.getElementById("operand1").addEventListener('keyup', function(){checkValues(this,0)}, false);
+  document.getElementById("operand1").addEventListener('keyup', function(){checkValues(this,1)}, false);
 
 }
 
