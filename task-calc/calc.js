d20105b87880f49eb134e7b52d436e0abd15c4f7
Evgeny Kravchenko <evgeniy.kravtschenko@gmail.com>
2013-01-31 18:08:06 -0500

    Real MVC architecture

--- task-calc/calc.js
+++ task-calc/calc.js
@@ -1,47 +1,20 @@
 function Calc() {
     
-    this.add = function() {
-      this.action("+");
+    this.add = function(a, b) {
+      return a + b;
     }
 
-    this.sub = function() {
-      this.action("-");
+    this.sub = function(a, b) {
+      return a - b;
     }
 
-    this.mul = function() {
-      this.action("*");
+    this.mul = function(a, b) {
+      return a * b;
     }
 
-    this.div = function() {
-      this.action("/");
-    }
-
-    this.action = function(action) {
-      this.get_values();
-
-      switch (action) {
-        case '+': this.result = this.operand1 + this.operand2;  break;
-        case '-': this.result = this.operand1 - this.operand2;  break;
-        case '*': this.result = this.operand1 * this.operand2;  break;
-        case '/': this.result = this.operand1 / this.operand2;  break;
-      }
-
-      this.view_result();
-    }
-
-    this.get_values = function() {
-      this.operand1 = parseFloat(document.getElementById("operand1").value);
-      this.operand2 = parseFloat(document.getElementById("operand2").value);
-    }
-
-    this.view_result = function() {
-      this.validate_result();
-      document.getElementById("result").value = this.result;
-    }
-
-    this.validate_result = function() {
-      this.result = (this.result === Infinity || isNaN(this.result) === true) ? 'Div by zero!': this.result.toFixed(2);
-    }
+    this.div = function(a, b) {
+      return a / b;
+    }   
     
     return this;
   }
